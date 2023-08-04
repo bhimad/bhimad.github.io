250d9a530ccc13dd838c85644228b9e8fc48c525
Kiran Shahi <kiran.shahi.np@gmail.com>
2023-08-04 14:09:41 +0100

    map script updated

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 var mymap = L.map('mapid').setView([27.9788, 84.0793], 16);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-	maxZoom: 18,
-	id: 'mapbox/streets-v11',
-	tileSize: 512,
-	zoomOffset: -1
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '© OpenStreetMap',
+	// tileSize: 512,
+	// zoomOffset: -1
 }).addTo(mymap);
